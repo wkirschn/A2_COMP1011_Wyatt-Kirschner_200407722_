e1d760fff4da50c49975225c60710d75c90a96e1
Wyatt <wkirschn@uoguelph.ca>
2020-12-08 14:13:52 -0500

    Starting

---
+++ src/Utilities/FoodUtility.java
@@ -0,0 +1,20 @@
+package Utilities;
+// API KEY: 5fec8b624e914a81b3e359d38938c941 // https://api.spoonacular.com/?apiKey=5fec8b624e914a81b3e359d38938c941
+// The initial request is ?, followed by &
+// https://api.spoonacular.com/recipes/716429/information?apiKey=5fec8b624e914a81b3e359d38938c941&includeNutrition=true
+// https://api.spoonacular.com/food/ingredients/search?query=banana&number=2&sort=calories&sortDirection=desc&apiKey=5fec8b624e914a81b3e359d38938c941
+// https://api.spoonacular.com/food/ingredients/search?query=banana&sortDirection=desc&apiKey=5fec8b624e914a81b3e359d38938c941
+// https://api.spoonacular.com/food/ingredients/9040/information?amount=1&apiKey=5fec8b624e914a81b3e359d38938c941 -- Can grab by ID and insert into the Query
+// Grab the CaloricBreakdown which includes percentProtein, percentFat, percentCarbs... nutrition, nutrients, Calories, Fat, Carbs, Protein
+//https://api.spoonacular.com/food/ingredients/search?query=cookie&apiKey=5fec8b624e914a81b3e359d38938c941
+
+//  https://api.spoonacular.com/food/ingredients/search?apiKey=5fec8b624e914a81b3e359d38938c941
+
+// If the JSON file returns blank or total results  = 0, then it won't add anything to the program
+
+
+public class FoodUtility {
+
+    public static ArrayList<>
+
+}
