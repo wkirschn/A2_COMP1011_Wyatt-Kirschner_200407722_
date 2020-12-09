1388209afc61a4531aa022fb1e15379726895b74
Wyatt <wkirschn@uoguelph.ca>
2020-12-09 18:34:20 -0500

    Done the Main View, time to ensure my JSON and API can retrieve information properly

--- src/Controllers/searchFoodController.java
+++ src/Controllers/searchFoodController.java
@@ -1,11 +1,37 @@
 package Controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class searchFoodController {
 
     @FXML
     private VBox leftVBox;
+
+    @FXML
+    private Label searchLabel;
+
+    @FXML
+    private Label totalResultsLabel;
+
+    @FXML
+    private Label pageLabel;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private Label foodLabel;
+
+    @FXML
+    private Label servingLabel;
+
+    @FXML
+    private Label calorieLabel;
+
+    @FXML
+    private Button moreInformationButton;
 
 }
